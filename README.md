9fec6777e6a88275820381e6ebaa4ffde9ce4f64
dorsha <doron@dorsha.com>
2015-03-29 16:59:56 +0300

    score-cli to cslang-cli

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
-# score-cli 
+# cslang-cli
 
 <img align="right" src="http://www.openscore.io/images/logos/score_logo_50x50.b6824b20.png">
 
-> The score command line interface.
+> The CloudSlang command line interface.
 
-Install this globally and you'll have access to the `slang` command anywhere on your system.
+Install this globally and you'll have access to the `cslang` command anywhere on your system.
 
 ```shell
-npm install -g score-cli
+npm install -g cslang-cli
 ```
 
-Now you can just use the `slang` command anywhere
+Now you can just use the `cslang` command anywhere
 
 ```shell
 slang
 ```
 
-Refer to [openscore](http://openscore.io) website for more information.
+Refer to [CloudSlang](http://cloudslang.io) website for more information.
 
 # Prerequisite
 Node.js & Java installed.
 
-score-cli page in the [NPM repository](https://www.npmjs.com/package/score-cli).
+cslang-cli page in the [NPM repository](https://www.npmjs.com/package/cslang-cli).
 
 ## Linux issues
 It might be needed to run npm like the following:
 
 ```shell
-sudo npm install -g score-cli
+sudo npm install -g cslang-cli
 ```
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log("Getting score... it might take a while...");
+console.log("Getting cloud slang... it might take a while...");
 
 var fs = require("fs");
 var request = require("request");
 var AdmZip = require('adm-zip');
 var wrench = require("wrench");
 
-var scoreCliUrl = 'https://github.com/openscore/score-language/releases/download/slang-CLI-v0.2.1/score-lang-cli.zip';
-var scoreZip = 'score-lang-cli.zip';
-var scoreFolder = 'score';
+var cslangCliUrl = 'https://github.com/CloudSlang/cloud-slang/releases/download/score-language-0.7.0/slang-cli.zip';
+var cslangZip = 'cslang-cli.zip';
+var cslangFolder = 'cslang';
 
-//var re = request.defaults({'proxy':'http://web-proxy.isr.hp.com:8080'});
-//var r = re.get(scoreCliUrl).pipe(fs.createWriteStream(scoreZip));
-var r = request(scoreCliUrl).pipe(fs.createWriteStream(scoreZip));
+var r = request(cslangCliUrl).pipe(fs.createWriteStream(cslangZip));
 
 r.on('finish', function () {
-    var zip = new AdmZip(scoreZip);
-    zip.extractAllTo(scoreFolder, true);
-	wrench.chmodSyncRecursive(scoreFolder, '0755');
+    var zip = new AdmZip(cslangZip);
+    zip.extractAllTo(cslangFolder, true);
+	wrench.chmodSyncRecursive(cslangFolder, '0755');
 	console.log("Finished");
 });
